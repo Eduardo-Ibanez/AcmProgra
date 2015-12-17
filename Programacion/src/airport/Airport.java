303e6bd05fb8915e01a8534d8e71afd9d6650e2b
eduardo ibanez <eduardo.ilabrin@gmail.com>
2015-12-17 17:16:22 -0300

    ACM

--- Programacion/src/airport/Airport.java
+++ Programacion/src/airport/Airport.java
@@ -1,22 +1,47 @@
 package airport;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Airport {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] seats = new int[m];
+        for (int i = 0; i < seats.length; i++) {
+          seats[i] = sc.nextInt();
+        }
+        sc.close();
+     
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+        for (int i = 0; i < seats.length; i++) {
+          if (seats[i] != 0) {
+            maxHeap.add(-seats[i]);
+            minHeap.add(seats[i]);
+          }
+        }
+     
+        long max = 0;
+        for (int i = 0; i < n; i++) {
+          int t = -maxHeap.poll();
+          max += t;
+          if (t - 1 != 0)
+            maxHeap.add(-(t - 1));
+        }
+     
+        long min = 0;
+        for (int i = 0; i < n; i++) {
+          int t = minHeap.poll();
+          min += t;
+          if (t - 1 != 0)
+            minHeap.add(t - 1);
+        }
+     
+        System.out.println(max + " " + min);
 
-		Scanner entrada=new Scanner (System.in);
-		
-		int pasajeros=entrada.nextInt();
-		int aviones=entrada.nextInt();
-		
-		for(int i=0;i<pasajeros;i++){
-			//sumatoria para encontrar el minimo
-			//manual para encontara el maximo
-		}
-
-	}
+    }
 
 }
